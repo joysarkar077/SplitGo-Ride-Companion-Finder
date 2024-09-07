75dd123a0cfb7dd5377f50ed2e5bf2770effb9dc
IstiakImran <toufique.hassan.71@gmail.com>
2024-09-07 23:20:14 +0600

    database connection added

---
+++ src/lib/dbConnection.ts
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise";
+
+let connection: mysql.Connection | null;
+const dbConnection = async () => {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      port: Number(process.env.DATABASE_PORT),
+    });
+  }
+  return connection;
+};
+
+export default dbConnection;
