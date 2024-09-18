4c4a3a322ff52237d866827b3f27fde3c936e022
IstiakImran <toufique.hassan.71@gmail.com>
2024-09-18 17:00:32 +0600

    bug fixed for font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import AuthProvider from "./context/AuthProvider";
 import Navbar from "@/components/custionUi/Navbar";
 import 'leaflet/dist/leaflet.css';
-import {Raleway} from 'next/font/google';
+import { Raleway } from 'next/font/google';
 
 const raleway = Raleway({ subsets: ["latin"] });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "SplitGo",
