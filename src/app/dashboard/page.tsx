0ea7efdb6d625e285d5a7af29759c373d33b3415
IstiakImran <toufique.hassan.71@gmail.com>
2024-09-21 12:38:12 +0600

    trying to  fix map error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,10 @@
 'use client';
-// import dynamic from 'next/dynamic';
 import RideRequestForm from '@/components/custionUi/RideRequestForm';
-
-// const Map = dynamic(() => import('@/components/custionUi/Map'), {
-//   ssr: false,
-// });
 
 const Dashboard = () => {
   return (
     <div className='w-full'>
       <RideRequestForm />
-
     </div>
   );
 };
